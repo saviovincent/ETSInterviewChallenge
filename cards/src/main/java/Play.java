8f359569b61e0e68a035838639606bb528aebc3f
saviovincent <saviovincent24@gmail.com>
2018-02-28 01:19:02 -0500

    Added enums
    Added enums

--- cards/src/main/java/Play.java
+++ cards/src/main/java/Play.java
@@ -14,6 +14,41 @@ public class Play {
     private static final int RANDOMMIN = 0;
     private static int randomMax = 51;
 
+    public enum Suit{
+        HEART("Heart"),
+        SPADE("Spade"),
+        DIAMOND("Diamond"),
+        CLUB("Club");
+
+        private String suitName;
+
+        Suit(String suitName) {
+            this.suitName = suitName;
+        }
+
+        public String getSuitName() {
+            return suitName;
+        }
+    }
+
+    public enum Facecards {
+        JACK("Jack"),
+        KING("King"),
+        QUEEN("Queen"),
+        ACE("Ace");
+
+        private String faceValue;
+
+        Facecards(String faceValue) {
+            this.faceValue = faceValue;
+        }
+
+        public String getFaceValue() {
+            return faceValue;
+        }
+    }
+
+
     ArrayList<Card> deck = new ArrayList<>(52);
 
     /**
@@ -21,24 +56,24 @@ public class Play {
      */
     private void initialiseCards(){
 
-    ArrayList<Card> heartCards = new ArrayList<Card>(13);
-    ArrayList<Card> spadeCards = new ArrayList<Card>(13);
-    ArrayList<Card> diamondCards = new ArrayList<Card>(13);
-    ArrayList<Card> clubCards = new ArrayList<Card>(13);
+        ArrayList<Card> heartCards = new ArrayList<Card>(13);
+        ArrayList<Card> spadeCards = new ArrayList<Card>(13);
+        ArrayList<Card> diamondCards = new ArrayList<Card>(13);
+        ArrayList<Card> clubCards = new ArrayList<Card>(13);
 
-    populateCards("Heart",heartCards);
-    deck.addAll(heartCards);
+        populateCards(Suit.HEART.getSuitName(),heartCards);
+        deck.addAll(heartCards);
 
-    populateCards("Spade",spadeCards);
-    deck.addAll(spadeCards);
+        populateCards(Suit.SPADE.getSuitName(),spadeCards);
+        deck.addAll(spadeCards);
 
-    populateCards("Diamond",diamondCards);
-    deck.addAll(diamondCards);
+        populateCards(Suit.DIAMOND.getSuitName(),diamondCards);
+        deck.addAll(diamondCards);
 
-    populateCards("Club",clubCards);
-    deck.addAll(clubCards);
+        populateCards(Suit.CLUB.getSuitName(),clubCards);
+        deck.addAll(clubCards);
 
-}
+    }
 
     /**
      *
@@ -47,33 +82,33 @@ public class Play {
      */
     private void populateCards(String suit, ArrayList<Card> cardsList){
 
-    for(int i = CARDNUMERICSTARTVALUE; i <= CARDNUMERICENDVALUE; i++){
+        for(int i = CARDNUMERICSTARTVALUE; i <= CARDNUMERICENDVALUE; i++){
 
-        Card card = new Card( suit, Integer.toString(i));
-        cardsList.add(card);
+            Card card = new Card( suit, Integer.toString(i));
+            cardsList.add(card);
+        }
+
+        Card jack = new Card(suit, Facecards.JACK.getFaceValue());
+        Card king = new Card(suit, Facecards.KING.getFaceValue());
+        Card queen = new Card(suit, Facecards.QUEEN.getFaceValue());
+        Card ace = new Card(suit, Facecards.ACE.getFaceValue());
+
+        cardsList.add(jack);
+        cardsList.add(king);
+        cardsList.add(queen);
+        cardsList.add(ace);
+
     }
-
-    Card king = new Card(suit, "King");
-    Card queen = new Card(suit, "Queen");
-    Card jack = new Card(suit, "Jack");
-    Card ace = new Card(suit, "Ace");
-
-    cardsList.add(king);
-    cardsList.add(queen);
-    cardsList.add(jack);
-    cardsList.add(ace);
-
-}
 
     /**
      * Method to shuffle cards in a deck
      */
     private void shuffle(){
 
-    Collections.shuffle(deck);
+        Collections.shuffle(deck);
 
-    deck.forEach((card -> System.out.println("Card: "+card.getValue() + " of "+ card.getSuit())));
-}
+        deck.forEach((card -> System.out.println("Card: "+card.getValue() + " of "+ card.getSuit())));
+    }
 
     /**
      * Generates a random number and returns card dealt from deck
@@ -81,25 +116,25 @@ public class Play {
      */
     private Card dealOneCard(){
 
-    if(!deck.isEmpty()){
+        if(!deck.isEmpty()){
 
-        int random = (int)(Math.random() * ((randomMax - RANDOMMIN) + 1)) + RANDOMMIN;
+            int random = (int)(Math.random() * ((randomMax - RANDOMMIN) + 1)) + RANDOMMIN;
 
-        Card removedCard = deck.get(random);
+            Card removedCard = deck.get(random);
 
-        deck.remove(random);
+            deck.remove(random);
 
-        randomMax--;
+            randomMax--;
 
-        return removedCard;
+            return removedCard;
+        }
+
+        else {
+            System.out.println("Deck is empty.. No card to be removed..!!");
+
+            return null;
+        }
     }
-
-    else {
-        System.out.println("Deck is empty.. No card to be removed..!!");
-
-        return null;
-    }
-}
 
     /**
      * Main function
@@ -107,23 +142,23 @@ public class Play {
      */
     public static void main(String args[]){
 
-    Play play = new Play();
+        Play play = new Play();
 
-    play.initialiseCards();
+        play.initialiseCards();
 
-    play.shuffle();
+        play.shuffle();
 
-    for (int i = DECKSTARTINDEX; i <= DECKENDINDEX; i++){
+        for (int i = DECKSTARTINDEX; i <= DECKENDINDEX; i++){
 
-        Card dealt = play.dealOneCard();
+            Card dealt = play.dealOneCard();
 
-        if(null != dealt){
-            System.out.println("Card removed : "+ dealt.getValue()+ " of "+ dealt.getSuit());
+            if(null != dealt){
+                System.out.println("Card removed : "+ dealt.getValue()+ " of "+ dealt.getSuit());
+            }
         }
+
+        play.dealOneCard(); //Check for the 53rd call
+
     }
-
-    play.dealOneCard(); //Check for the 53rd call
-
-}
 
 }
